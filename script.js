eae22b761a99fd3a888a3b5d142edc4f756d3eea
Sellerie1209 <bauchhenri12@gmail.com>
2025-08-12 21:37:56 +0200

    modified:   index.html 	modified:   script.js 	modified:   style.css

---
+++ script.js
@@ -0,0 +1,54 @@
+let pointsa = 0;
+let pointsb = 0;
+let difference = 0;
+
+function add(team) {
+    if (team == 1) {
+        pointsa++;
+        document.getElementById("btnteama").value = pointsa;
+
+        let difference = pointsa - pointsb;
+
+        if (pointsa >= 25 & difference >= 2) {
+            win(team)
+        }
+    }
+
+    if (team == 2) {
+        pointsb++;
+        document.getElementById("btnteamb").value = pointsb;
+
+        let difference = pointsb - pointsa;
+
+        if (pointsb >= 25 & difference >= 2) {
+            win(team);
+        }
+    }
+
+}
+
+function subtract(team) {
+    if (team == a) {
+        pointsa--;
+        document.getElementById("btnteama").value = pointsa;
+    }
+
+    if (team == b) {
+        pointsb--;
+        document.getElementById("btnteamb").value = pointsb;
+    }
+}
+
+function win(team) {
+    pointsa = 0;
+    pointsb = 0;
+    document.getElementById("btnteama").value = pointsa;
+    document.getElementById("btnteamb").value = pointsb;
+    if (team == 1) {
+        document.getElementById("teamnamea").textContent = "Team A Winner";
+    }
+
+    if (team == 2) {
+        document.getElementById("teamnameb").textContent = "Team B Winner";
+    }
+}
