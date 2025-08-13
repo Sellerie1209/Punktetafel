fa275bd63423415bd63dd8641395ff44d934616c
Sellerie1209 <bauchhenri12@gmail.com>
2025-08-13 17:49:48 +0200

    modified:   index.html 	modified:   script.js 	modified:   style.css 	new file:   whisle.mp3

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let pointsa = 0;
 let pointsb = 0;
 let difference = 0;
+let gameend = false;
 
 function add(team) {
     if (team == 1) {
@@ -45,10 +46,25 @@ function win(team) {
     document.getElementById("btnteama").value = pointsa;
     document.getElementById("btnteamb").value = pointsb;
     if (team == 1) {
-        document.getElementById("teamnamea").textContent = "Team A Winner";
+        document.getElementById("teamnamea").textContent = "Team A Won";
+        document.getElementById("teamnameb").textContent = "Team B Lost";
     }
 
     if (team == 2) {
-        document.getElementById("teamnameb").textContent = "Team B Winner";
+        document.getElementById("teamnameb").textContent = "Team B Won";
+        document.getElementById("teamnamea").textContent = "Team A Lost";
+    }
+    gameend = true;
+}
+
+function whisle() {
+    let audio = document.getElementById("sound");
+    audio.currentTime = 0;
+    audio.play();
+
+    if (gameend == true) {
+        gameend = false;
+        document.getElementById("teamnamea").textContent = "Team A";
+        document.getElementById("teamnameb").textContent = "Team B";
     }
 }
